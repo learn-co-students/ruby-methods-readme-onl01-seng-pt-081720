da06bccfd456ccec183ae09b43846dbbfe6fe8d6
Ken Barlow <kenbrlow@gmail.com>
2020-08-20 03:03:15 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,44 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
+
+# ... The rest of the program
+
+say_hello_world_ten_times
+
+def greeting
+  puts "Hello World"
+end
+
+greeting # Executing the method by name
+#=> "Hello World"
+ 
+greeting # Executing the method again
+#=> "Hello World"
